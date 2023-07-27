ffe5f5d0334e0992fd1bf93eebd42923fd30745b
Saka <sakamoshood@ymail.com>
2023-07-26 22:18:25 -0400

    folder structure and clean up

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../public/globals.css";
+
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../public/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Heart of the Nile</h1>
+    </main>
+  );
+}
