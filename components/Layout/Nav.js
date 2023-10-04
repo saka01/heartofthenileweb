260bbcb62a5f6a9c3f8e1611af768e2bca4eab2d
Saka <sakamoshood@ymail.com>
2023-10-04 11:30:58 -0400

    Changed colors of nav for new design

--- components/Layout/Nav.js
+++ components/Layout/Nav.js
@@ -6,7 +6,7 @@ export const Nav = {
     return (
       <Link href={`${url}`}>
         <span
-          className={`text-white capitalize ${
+          className={`text-red-600 capitalize ${
             active === title ? "font-semibold" : ""
           } cursor-pointer text-xl ml-5 hover:font-semibold `}
         >
@@ -19,7 +19,7 @@ export const Nav = {
     return (
       <button
         onClick={handleClick}
-        className={`md:hidden text-5xl text-gray-200 hover:text-white`}
+        className={`md:hidden text-5xl text-red-500 hover:text-red`}
       >
         <i className="bi bi-list"></i>
       </button>
@@ -28,7 +28,7 @@ export const Nav = {
   Btn: ({ url, label }) => {
     return (
       <Link href={`${url}`}>
-        <span className="ml-5 w-44  bg-white py-12 text-xl px-10 rounded-2xl shadow text-gray-900 hover:text-[#CE2829] cursor-pointer capitalize font-semibold">
+        <span className="ml-5 w-44  bg-white py-12 text-xl px-10 rounded-2xl shadow text-green-500 hover:text-red-500 cursor-pointer capitalize font-semibold">
           {label} <i className="bi bi-cart-fill"></i>
         </span>
       </Link>
@@ -38,7 +38,7 @@ export const Nav = {
     return (
       <Link href={`${url}`}>
         <span
-          className={`text-white ${
+          className={`text-red-500 ${
             active === title ? "font-semibold" : ""
           } cursor-pointer capitalize text-xl mb-10 hover:font-semibold`}
         >
@@ -50,7 +50,7 @@ export const Nav = {
   MobileBtn: ({ label }) => {
     return (
       <Link href={"#"}>
-        <span className="w-40 text-center text-xl bg-white py-12 px-10 rounded-2xl shadow text-gray-900 hover:text-[#CE2829] cursor-pointer font-semibold capitalize">
+        <span className="w-40 text-center text-xl bg-white py-12 px-10 rounded-2xl shadow text-red-900 hover:text-[#CE2829] cursor-pointer font-semibold capitalize">
           {label} <i className="bi bi-cart-fill"></i>
         </span>
       </Link>
@@ -61,7 +61,7 @@ export const Nav = {
       <div className="w-full flex flex-col md:flex-row items-end md:items-center md:justify-end px-24 pt-4">
         <a
           href="#"
-          className="flex items-center text-gray-100 text-sm hover:font-semibold hover:text-white"
+          className="flex items-center text-red-600 text-sm hover:font-semibold hover:text-red"
         >
           <i className="bi bi-geo-alt-fill"></i>
           <span className="ml-1 tracking-wide">
@@ -70,7 +70,7 @@ export const Nav = {
         </a>
         <a
           href="tel:#"
-          className="ml-5 flex items-center text-gray-100 text-sm hover:font-semibold hover:text-white"
+          className="ml-5 flex items-center text-red-600 text-sm hover:font-semibold hover:text-red"
         >
           <i className="bi bi-telephone-fill"></i>
           <span className="ml-1 tracking-wide">+1 (519) 432-3222</span>
