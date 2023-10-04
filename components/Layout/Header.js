549698f256237104f688334d372ea3eadf35cc13
Saka <sakamoshood@ymail.com>
2023-10-04 11:17:46 -0400

    changed nav bg to white

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -12,7 +12,7 @@ function Header({ signoutHandler, role, page }) {
   const [isToggled, setToggled] = useState(false);
   const toggleTrueFalse = () => setToggled(!isToggled);
   return (
-    <header className="bg-[#CE2829] sticky-top shadow-md">
+    <header className="bg-white sticky-top shadow-md">
       <div className="relative">
         <Nav.TopBar />
         <nav
@@ -39,7 +39,7 @@ function Header({ signoutHandler, role, page }) {
         <div
           className={`${
             isToggled ? "flex flex-col" : "hidden"
-          } absolute md:hidden px-24 bg-[#CE2829] w-full pb-20`}
+          } absolute md:hidden px-24 bg-white w-full pb-20`}
         >
           {navLinks.map((item) => {
             return (
@@ -55,54 +55,6 @@ function Header({ signoutHandler, role, page }) {
         </div>
       </div>
     </header>
-    // <>
-
-    //     <div className="header">
-    //         <div className="container">
-    //             <div className="row">
-    //                 <div className="col-xl-12">
-    //                     <div className="navigation">
-    //                         <nav className="navbar navbar-expand-lg navbar-light">
-    //                             <div className="brand-logo">
-    //                                 <Link href="/"><a>
-    //                                     <img src="/images/logo.png" alt="" className="logo-primary" />
-    //                                 </a></Link>
-    //                             </div>
-    //                             {/* <div className="search">
-    //                                 <form>
-    //                                     <span><i className="ri-search-line"></i></span>
-    //                                     <input type="text" placeholder="Search Here" />
-    //                                 </form>
-    //                             </div> */}
-    //                             <button className="navbar-toggler" type="button" onClick={toggleTrueFalse}>
-    //                                 <span className="navbar-toggler-icon"></span>
-    //                             </button>
-    //                             <div className={isToggled ? "collapse navbar-collapse show" : "collapse navbar-collapse"}>
-    //                                 <ul className="navbar-nav ms-auto">
-
-    //                                     <li className="nav-item dropdown">
-    //                                         <Link href="/"><a className="nav-link">Home
-    //                                         </a>
-    //                                         </Link>
-    //                                     </li>
-    //                                     <Link href="/cart" passHref>
-    //                                         <a className="mx-20"><CartCounter /></a>
-    //                                     </Link>
-
-    //                                 </ul>
-    //                             </div>
-
-    //                             <div className="signin-btn">
-    //                                 <a className="btn btn-primary" onClick={logoutHandler}>Sign out</a>
-    //                             </div>
-    //                         </nav>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-
-    // </>
   );
 }
 export default Header;
